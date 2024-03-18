a7c7edcb7ee3334fe63572aecde96ddfbb2a2c30
akanshacev <akanshacev@gmail.com>
2024-03-18 12:01:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import CounterMain1 from './component/CounterMain1'
+
+function App() {
+  
+
+  return (
+    <>
+   <CounterMain1/>
+    
+    </>
+    
+  )
+}
+
+
+
+export default App
